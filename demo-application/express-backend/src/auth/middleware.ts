c40307ae0d7af2a1d79dd7183e31deadd381df48
stzagkarak <stzagkarak@gmail.com>
2025-07-22 20:54:28 +0300

    Update-CleanUp-1

--- demo-application/express-backend/src/auth/middleware.ts
+++ demo-application/express-backend/src/auth/middleware.ts
@@ -1,17 +1,20 @@
-import { Request, Response , NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 
-export async function isAuthenticated(req:Request, res:Response, next:NextFunction) {
-    if(req.isAuthenticated()) return next();
-    return next(401);
+export async function isAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.isAuthenticated()) return next();
+  return next(401);
 }
 
-export  async function isAdmin(req:Request, res:Response, next:NextFunction) {
-    try {
-        // @ts-ignore
-        if(req.user?.["user-role"] === 'admin') return next();
-    }
-    catch(err) {
-        return next(401);
-    }
+export async function isAdmin(req: Request, res: Response, next: NextFunction) {
+  try {
+    // @ts-ignore
+    if (req.user?.["roles"].includes("admin")) return next();
+  } catch (err) {
     return next(401);
+  }
+  return next(401);
 }
--- keycloak-compose-local/run_compose.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# $1 -> -d to run compose detached 
-mkdir -p instance/database/mbackups
-
-docker-compose down --remove-orphans
-docker-compose up $1
