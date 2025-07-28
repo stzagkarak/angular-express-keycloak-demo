b253ab1fe457517d19d25d54262a811e998df13e
stzagkarak <stzagkarak@gmail.com>
2025-07-28 11:39:35 +0300

    prod script fix

--- simple-demo-app/run_prod.sh
+++ simple-demo-app/run_prod.sh
@@ -3,7 +3,7 @@
 # $2 -> -d to run compose detached
 
 #mv .env development.env # rename .env file to keep it from being used in docker-compose file 
-cp angular.prod.environment.ts angular-frontend/src/environments/environment.ts
+cp angular.prod.environment.ts ./backend/angular-frontend/src/environments/environment.ts
 
 docker compose down --remove-orphans
 docker compose up $1 $2
